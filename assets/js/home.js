4dfc0ea3df6338d55582caabce592795ec251007
Dan Boyle <danboyle8637@gmail.com>
2020-06-15 14:45:46 -0400

    Building lazy loading images and art direction for post images.

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,10 @@
 const tagIcons = document.querySelectorAll(".post-card-tag-container");
+const cardImages = document.querySelectorAll(".blog-feature-image");
+
+const blurUpImage = (imgElement) => {
+  imgElement.src = imgElement.dataset.src;
+  imgElement.style.filter = "blur(0px)";
+};
 
 if (tagIcons) {
   const tagArray = Array.from(tagIcons);
@@ -8,4 +14,9 @@ if (tagIcons) {
       console.log("Reveal tags");
     });
   });
+
+  const cardImagesArray = Array.from(cardImages);
+  const image = cardImagesArray[0];
+
+  blurUpImage(image);
 }
