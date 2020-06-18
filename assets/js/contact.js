e2a142ad0219ae4bc05110e578c0231113486e36
Dan Boyle <danboyle8637@gmail.com>
2020-06-18 15:09:05 -0400

    Added Netlify large images and going to hook up forestry and give this a test.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -8,12 +8,34 @@ let firstName = "";
 let emailAddress = "";
 let message = "";
 
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
 if (contactForm) {
   contactForm.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    const form = event.target;
+
     console.log("first name: ", firstName);
     console.log("email address: ", emailAddress);
     console.log("message: ", message);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": "contact",
+        firstName: firstName,
+        emailAddress: emailAddress,
+        message: message,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch((error) => alert(error));
   });
 
   nameField.addEventListener("input", (event) => {
