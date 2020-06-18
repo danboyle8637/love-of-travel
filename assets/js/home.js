7f30360af6850f2b60024804a3994161c1f335d2
Dan Boyle <danboyle8637@gmail.com>
2020-06-18 08:41:29 -0400

    Fixed the logos and also some css to set defaults for lists and anchor tags. Now working on the last animations and pagination and large media storage.

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,20 @@
 const tagIcons = document.querySelectorAll(".post-card-tag-container");
+const tagLabels = document.querySelectorAll(".blog-tag-label");
 const cardImages = document.querySelectorAll(".blog-feature-image");
 
 const tagArray = Array.from(tagIcons);
+const tagLabelArray = Array.from(tagLabels);
+
+console.log(tagLabelArray);
+
+gsap.set(tagLabelArray, { opacity: 0, x: 300 });
 
 tagArray.forEach((tag) => {
   tag.addEventListener("mouseenter", () => {
     console.log("Reveal tags");
+    tagLabelArray.forEach((label) => {
+      gsap.to(label, { opacity: 1, x: 0 });
+    });
   });
 });
 
