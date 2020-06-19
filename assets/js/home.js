e3a765f6ee61aa592813d18ea891982e6dd93ef9
Dev Dan <devdan@macbook-pro.lan>
2020-06-19 15:04:42 -0400

    Have the tags doing almost what I want them to be doing on mouse over. Just need to figure out how to handle it on a mobile device.

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,20 +1,51 @@
-const tagIcons = document.querySelectorAll(".post-card-tag-container");
+const tagIconContainers = document.querySelectorAll(".post-card-tag-container");
 const tagLabels = document.querySelectorAll(".blog-tag-label");
 const cardImages = document.querySelectorAll(".blog-feature-image");
 
-const tagArray = Array.from(tagIcons);
+const tagArray = Array.from(tagIconContainers);
 const tagLabelArray = Array.from(tagLabels);
 
-console.log(tagLabelArray);
+tagLabelArray.forEach(label => {
+  label.style.display = "none"
+})
 
 gsap.set(tagLabelArray, { opacity: 0, x: 300 });
 
 tagArray.forEach((tag) => {
   tag.addEventListener("mouseenter", () => {
-    console.log("Reveal tags");
-    tagLabelArray.forEach((label) => {
-      gsap.to(label, { opacity: 1, x: 0 });
-    });
+    gsap.to(tag, {
+      duration: 0.5,
+      left: 20,
+      ease: Back.easeOut
+    })
+
+    const childNodesArray = Array.from(tag.childNodes)
+
+    childNodesArray.forEach(node => {
+      if (node.classList && node.classList.value === "blog-tag-label") {
+        node.style.display = "block"
+        gsap.to(node, { opacity: 1, x: 0, });
+      }
+    })
+
+  });
+
+  tag.addEventListener("mouseleave", () => {
+    gsap.to(tag, {
+      duration: 0.5,
+      left: 356,
+      ease: Back.easeOut
+    })
+
+    const childNodesArray = Array.from(tag.childNodes)
+
+    childNodesArray.forEach(node => {
+      if (node.classList && node.classList.value === "blog-tag-label") {
+        gsap.to(node, { opacity: 0, x: 300, });
+        node.style.display = "none"
+      }
+    })
+
   });
 });
 
